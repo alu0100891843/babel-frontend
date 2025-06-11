a2adccba9603df1fd8f49829462ad60f69be8979
Cristian Manuel Ángel Díaz <alu0100891843@ull.edu.es>
2025-06-11 14:43:48 +0100

    Improvements of generic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CandidatesComponent, MatMenuModule, MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [CandidatesComponent, MatMenuModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule],
   providers: [CandidatesService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
