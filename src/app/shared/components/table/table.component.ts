5191228630ff2608cd265dbbb4ed2028d50b2bc3
Cristian Manuel Ángel Díaz <alu0100891843@ull.edu.es>
2025-06-13 17:15:36 +0100

    Improve functionality of table component (sort and filter)

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 export interface TableDefinitionType {
   title: string;
@@ -22,12 +26,15 @@ export interface TableDefinitionType {
   standalone: true,
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
-  imports: [MatTableModule, MatButtonModule, MatToolbarModule, MatMenuModule, MatPaginatorModule, MatIconModule, MatProgressSpinnerModule, CommonModule],
+  imports: [MatTableModule, MatSortModule, MatInputModule, MatFormFieldModule, MatTooltipModule, MatButtonModule, MatToolbarModule, MatMenuModule, MatPaginatorModule, MatIconModule, MatProgressSpinnerModule, CommonModule],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableComponent<T> implements OnInit, AfterViewInit, OnChanges {
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
 
   @Input()
   public tableDefinition!: TableDefinitionType;
@@ -38,18 +45,22 @@ export class TableComponent<T> implements OnInit, AfterViewInit, OnChanges {
 
   public dataSource = new MatTableDataSource<T>([]);
   public displayedColumns!: string[];
-
   public componentInitialized = false;
 
   ngOnInit() {
-
     this.displayedColumns = this.tableDefinition.columns.map(x => x.key);
     this.updateDataSource();
     this.componentInitialized = true;
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+
+    if (this.sort) {
+      this.dataSource.sort = this.sort;
+    }
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -59,11 +70,16 @@ export class TableComponent<T> implements OnInit, AfterViewInit, OnChanges {
     }
   }
 
-  private updateDataSource() {
-    this.dataSource.data = this.data as T[] || [];
-  }
-
   onAddButtonClick(): void {
     this.addAction.emit();
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
+    this.dataSource.filter = filterValue;
+  }
+
+  private updateDataSource() {
+    this.dataSource.data = this.data as T[] || [];
   }
 }
